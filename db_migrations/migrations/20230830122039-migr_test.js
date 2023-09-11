e2ccc73adf9ed12a40cc58d7b52123923c1b0648
Naveen Singh <naveen09cool@gmail.com>
2023-09-11 18:45:24 +0530

    added default current time

--- db_migrations/migrations/20230830122039-migr_test.js
+++ db_migrations/migrations/20230830122039-migr_test.js
@@ -18,16 +18,16 @@ module.exports = {
       },
       testDel:{
         type: Sequelize.STRING
-      }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
     })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('migr_test');
   }
 };
